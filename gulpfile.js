e3b73b3e940b2a772921a51dde5102049f8eb00f
Cristian Sanchez <cris1411@gmail.com>
2024-05-06 20:34:22 -0300

    Proyecto FestivalMusica con Gulp5 y mejor dieño

---
+++ gulpfile.js
@@ -0,0 +1,27 @@
+import { src, dest, watch, series } from 'gulp'
+import * as dartSass from 'sass'
+import gulpSass from 'gulp-sass'
+
+const sass = gulpSass(dartSass)
+
+export function js(done) {
+    src('src/js/app.js')
+        .pipe(dest('build/js'))
+
+    done();
+}
+
+export function css(done) {
+    src('src/scss/app.scss', {sourcemaps: true})
+        .pipe(sass().on('error', sass.logError))
+        .pipe(dest('build/css', {sourcemaps:'.'}))
+
+    done()
+}
+
+export function dev(){
+    watch('src/scss/**/*.scss', css)
+    watch('src/js/**/*.js', js)
+}
+
+export default series(js, css, dev)
